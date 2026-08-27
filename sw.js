e119db8cb2599ec80c36d495ec700c8f75f38000
Lip777 <66442781+Lip777@users.noreply.github.com>
2026-08-28 02:02:09 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const cacheName = "greeting-v1";
+
+const files = [
+  "./",
+  "index.html",
+  "manifest.json",
+  "music.mp3",
+  "bg1.jpg",
+  "bg2.jpg",
+  "bg3.jpg",
+  "icon-512.png"
+];
+
+// Установка: сохранения файлов в кэш
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(files))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Активация
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
+// Перехват запросов (работа оффлайн)
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
+  );
+});
